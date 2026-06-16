93c18801bf13b14c721ba77aabcbe2e5590fdca6
axxeler8 <marthey10@gmail.com>
2026-06-16 19:42:56 -0400

    lab1: solo archivos del Laboratorio 1

--- test_epuck.py
+++
@@ -1,7 +0,0 @@
-from controller import Robot
-import sys
-robot = Robot()
-timestep = int(robot.getBasicTimeStep())
-
-# We can't use GPS if it's not enabled, but we can check if it exists or just use a Supervisor
-# Wait, a normal controller can't access Supervisor unless it's a supervisor.
